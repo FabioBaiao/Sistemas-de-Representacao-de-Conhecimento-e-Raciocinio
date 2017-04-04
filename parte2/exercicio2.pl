6b65a46c6bde852b72b9195278f2749f26629c9e
João Pereira <joaopereira_19@hotmail.com>
2017-04-05 00:36:44 +0100

    adicionei ficheiros iniciais da parte 2

---
+++ parte2/exercicio2.pl
@@ -0,0 +1,39 @@
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% SIST. REPR. CONHECIMENTO E RACIOCINIO - MiEI/3
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Base de Conhecimento com informacao genealogica.
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% SICStus PROLOG: definicoes iniciais
+:- op( 900, xfy, '&' ). % operador para conjuncao de questoes
+:- op( 900, xfy, '::' ).
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Extensao do meta-predicado nao:: Questão -> {V,F}
+nao( Q ) :- Q, !, fail.
+nao( Q ).
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Extensao do meta-predicado demoC :: Conjunção, Resposta -> {V,F}
+demoC( P & X, verdadeiro ) :- demo( P, verdadeiro ), demoC( X, verdadeiro ).
+demoC( P & X, falso ) :- demo( P, falso ).
+demoC( P & X, falso ) :- demoC( X, falso ).
+demoC( P & X, desconhecido ) :- demo( P, desconhecido ), demoC( X, desconhecido ).
+demoC( P & X, desconhecido ) :- demo( P, desconhecido ), demoC( X, verdadeiro ).
+demoC( P & X, desconhecido ) :- demo( P, verdadeiro ), demoC( X, desconhecido ).
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Extensao do meta-predicado demo :: Conjunção, Resposta -> {V,F}
+demo( P , verdadeiro ) :- P.
+demo( P, falso ) :- -P.
+demo( P, desconhecido ) :- nao( P ), nao( -P ).
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Extensao do meta-predicado evolucao :: ?? 
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Extensao do meta-predicado involucao :: ?? 
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Extensao do predicado solucoes :: ?? 
