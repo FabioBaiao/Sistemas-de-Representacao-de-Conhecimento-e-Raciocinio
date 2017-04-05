dc10950ac097c7c4e3abad863f7314d033e1f04c
João Pereira <joaopereira_19@hotmail.com>
2017-04-05 14:39:04 +0100

    versao corrigida do demo

--- parte2/exercicio2.pl
+++ parte2/exercicio2.pl
@@ -5,9 +5,37 @@
 % Base de Conhecimento com informacao genealogica.
 
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% SICStus PROLOG: declaracoes iniciais
+
+:- set_prolog_flag(discontiguous_warnings, off).
+:- set_prolog_flag(single_var_warnings, off).
+:- set_prolog_flag(unknown, fail).
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
 % SICStus PROLOG: definicoes iniciais
 :- op( 900, xfy, '&' ). % operador para conjuncao de questoes
 :- op( 900, xfy, '::' ).
+
+% :- dynamic utente/4.
+% :- dynamic cuidado_prestado/4.
+% :- dynamic ato_medico/4.
+
+% :- dynamic profissional/4.
+% :- dynamic atribuido/2.
+% :- dynamic ato_medico/5.
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Extensao do predicado solucoes: F, Q, S -> {V,F}
+solucoes(F,Q,S) :- demo( Q, verdadeiro ), assert( tmp(F) ), fail.
+solucoes(F,Q,S) :- construir( S, [] ).
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Extensao do predicado construir: S1,S2 -> {V,F}
+construir(S1, S2) :-
+	retract(tmp(X)), !, construir(S1, [X|S2]).
+construir(S,S).
+
+
 
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
 % Extensao do meta-predicado utente :: IdUt, Nome, Idade, Morada -> {V,F,D}
@@ -24,20 +52,17 @@ nao( Q ) :- Q, !, fail.
 nao( Q ).
 
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
-% Extensao do meta-predicado demoC :: Conjunção, Resposta -> {V,F}
-demoC( P & X, verdadeiro ) :- demo( P, verdadeiro ), demoC( X, verdadeiro ).
-demoC( P & X, falso ) :- demo( P, falso ).
-demoC( P & X, falso ) :- demoC( X, falso ).
-demoC( P & X, desconhecido ) :- demo( P, desconhecido ), demoC( X, desconhecido ).
-demoC( P & X, desconhecido ) :- demo( P, desconhecido ), demoC( X, verdadeiro ).
-demoC( P & X, desconhecido ) :- demo( P, verdadeiro ), demoC( X, desconhecido ).
-
-%--------------------------------- - - - - - - - - - -  -  -  -  -   -
 % Extensao do meta-predicado demo :: Conjunção, Resposta -> {V,F}
+demo( P & X, verdadeiro ) :- demo( P, verdadeiro ), demo( X, verdadeiro ).
+demo( P & X, falso ) :- demo( P, falso ).
+demo( P & X, falso ) :- demo( X, falso ).
+demo( P & X, desconhecido ) :- demo( P, desconhecido ), demo( X, desconhecido ).
+demo( P & X, desconhecido ) :- demo( P, desconhecido ), demo( X, verdadeiro ).
+demo( P & X, desconhecido ) :- demo( P, verdadeiro ), demo( X, desconhecido ).
+
 demo( P , verdadeiro ) :- P.
 demo( P, falso ) :- -P.
 demo( P, desconhecido ) :- nao( P ), nao( -P ).
-
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
 % Extensao do meta-predicado evolucao :: ?? 
 
@@ -45,4 +70,4 @@ demo( P, desconhecido ) :- nao( P ), nao( -P ).
 % Extensao do meta-predicado involucao :: ?? 
 
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
-% Extensao do predicado solucoes :: ?? 
+% Extensao do predicado solucoes :: Predicado  
