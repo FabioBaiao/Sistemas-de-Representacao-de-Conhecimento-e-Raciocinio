443559e07f2068f4f4e7dfbd92c915434e0a2dd3
Manel18 <peaceandhendrix@gmail.com>
2017-05-21 16:17:29 +0100

    minor update

--- parte3/script.r
+++ parte3/script.r
@@ -13,11 +13,11 @@ library(arules)
 # formula variables should be changed after calling regsubsets(form, dataset, nvmax=8) after running conversion.r
 #formula <- FatigueLevel ~ Performance.MAMean + Performance.AEDMean + Performance.DDCMean + Performance.DMSMean
 #formulaFL <- FatigueLevel ~ Performance.KDTMean + Performance.MAMean + Performance.AEDMean + Performance.DDCMean + Performance.DMSMean
-formulaT <- Performance.Task ~ Performance.KDTMean + Performance.MAMean + Performance.DDCMean + Performance.DMSMean + Performance.MVMean
+formulaT <- Performance.Task ~ Performance.KDTMean + Performance.DDCMean + Performance.MAMean + Performance.DMSMean + Performance.MVMean
 task1 <- neuralnet(formulaT, train, hidden=c(7,3), lifesign = "FULL",algorithm="rprop+",linear.output = FALSE,threshold = 0.03)
 
 # get a subset of test set (only de columns of the variables used in formula)
-test.01 <- subset(test, select=c("Performance.KDTMean","Performance.MAMean", "Performance.DDCMean", "Performance.DMSMean", "Performance.MVMean"))
+test.01 <- subset(test, select=c("Performance.KDTMean", "Performance.DDCMean","Performance.MAMean", "Performance.DMSMean", "Performance.MVMean"))
 task1.resultados <- compute(task1, test.01)
 resultados <- data.frame(atual=test$Performance.Task, previsao=(task1.resultados$net.result*2)+1)
 resultados$previsao <- round(resultados$previsao, digits=0)
